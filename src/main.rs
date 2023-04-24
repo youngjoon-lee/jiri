70f43ecd7c9f39e1524000adacfa9ca91391398e
Youngjoon Lee <taxihighway@gmail.com>
2023-04-24 15:08:28 +0900

    feat: init libp2p swarm with default configs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use crate::p2p::new;
+
+mod p2p;
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
+    log::info!("Initializing JIRI node...");
+
+    let client = new().await?;
+    log::info!("Client:{client:?}");
+
+    Ok(())
 }
---
+++ src/p2p.rs
@@ -0,0 +1,66 @@
+use std::{
+    collections::hash_map::DefaultHasher,
+    error::Error,
+    hash::{Hash, Hasher},
+    time::Duration,
+};
+
+use futures::StreamExt;
+use libp2p::{
+    core::upgrade::Version,
+    gossipsub, identity, mdns, noise,
+    swarm::{NetworkBehaviour, SwarmBuilder, SwarmEvent},
+    tcp, yamux, Transport,
+};
+
+pub async fn new() -> Result<Client, Box<dyn Error>> {
+    let id_keys = identity::Keypair::generate_ed25519();
+    let peer_id = id_keys.public().to_peer_id();
+    log::info!("Peer {peer_id} generated");
+
+    let transport = tcp::async_io::Transport::default()
+        .upgrade(Version::V1Lazy)
+        .authenticate(noise::NoiseAuthenticated::xx(&id_keys)?)
+        .multiplex(yamux::YamuxConfig::default())
+        .boxed();
+
+    let message_id_fn = |message: &gossipsub::Message| {
+        let mut hasher = DefaultHasher::new();
+        message.data.hash(&mut hasher);
+        gossipsub::MessageId::from(hasher.finish().to_string())
+    };
+
+    let mut gossipsub = gossipsub::Behaviour::new(
+        gossipsub::MessageAuthenticity::Signed(id_keys),
+        gossipsub::ConfigBuilder::default()
+            .heartbeat_interval(Duration::from_secs(3))
+            .validation_mode(gossipsub::ValidationMode::Strict)
+            .message_id_fn(message_id_fn)
+            .build()?,
+    )?;
+    gossipsub.subscribe(&gossipsub::IdentTopic::new("jiri-chat"))?;
+
+    let mdns = mdns::async_io::Behaviour::new(mdns::Config::default(), peer_id)?;
+
+    let behaviour = Behaviour { gossipsub, mdns };
+
+    let mut swarm = SwarmBuilder::with_async_std_executor(transport, behaviour, peer_id).build();
+
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr { address, .. } => log::info!("Listening on {address:?}"),
+            _ => {}
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Client;
+
+#[derive(NetworkBehaviour)]
+struct Behaviour {
+    gossipsub: gossipsub::Behaviour,
+    mdns: mdns::async_io::Behaviour,
+}
