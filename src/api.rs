f6d5417b79832e6353a5478461072da63613b865
Youngjoon Lee <taxihighway@gmail.com>
2023-04-24 23:43:48 +0900

    feat: add websocket skeleton for api

--- src/api.rs
+++ src/api.rs
@@ -37,7 +37,9 @@ mod filters {
     pub fn all(
         command_sender: mpsc::Sender<Command>,
     ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
-        send_message(command_sender.clone()).or(status())
+        send_message(command_sender.clone())
+            .or(status())
+            .or(subscribe_messages())
     }
 
     pub fn send_message(
@@ -51,6 +53,13 @@ mod filters {
             .and_then(handlers::send_message)
     }
 
+    pub fn subscribe_messages(
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::path!("msg").and(warp::ws()).map(|ws: warp::ws::Ws| {
+            ws.on_upgrade(move |socket| handlers::subscribe_messages(socket))
+        })
+    }
+
     pub fn status() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
         warp::path!("status")
             .and(warp::get())
@@ -59,10 +68,13 @@ mod filters {
 }
 
 mod handlers {
-    use std::convert::Infallible;
+    use std::{convert::Infallible, time::Duration};
 
-    use futures::{channel::mpsc, SinkExt};
-    use warp::hyper::StatusCode;
+    use futures::{channel::mpsc, SinkExt, StreamExt};
+    use warp::{
+        hyper::StatusCode,
+        ws::{Message, WebSocket},
+    };
 
     use crate::p2p::Command;
 
@@ -78,6 +90,16 @@ mod handlers {
             return Ok(StatusCode::INTERNAL_SERVER_ERROR);
         }
         Ok(StatusCode::OK)
+    }
+
+    pub async fn subscribe_messages(ws: WebSocket) {
+        let (mut sender, ..) = ws.split();
+        loop {
+            tokio::time::sleep(Duration::from_secs(3)).await;
+            if let Err(e) = sender.send(Message::text("yo man")).await {
+                log::error!("Failed to send msg to WebSocket: {e:?}");
+            }
+        }
     }
 
     pub async fn status() -> Result<impl warp::Reply, Infallible> {
