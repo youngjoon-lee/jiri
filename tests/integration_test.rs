8e20a7624f07a8140060e10f86ba14f7c1c4badf
Youngjoon Lee <taxihighway@gmail.com>
2023-04-29 18:58:05 +0900

    remove comments

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,21 +8,10 @@ fn integration_test() {
     test.add_composition(jiri1);
     test.add_composition(jiri2);
 
-    // let has_ran: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
-    // let has_ran_test = has_ran.clone();
-
     test.run(|ops| async move {
         let jiri1 = ops.handle("jiri1");
         println!("jiri1: {jiri1:?}");
         let jiri2 = ops.handle("jiri2");
         println!("jiri2: {jiri2:?}");
-
-        // The container is in a running state at this point.
-        // Depending on the Image, it may exit on its own (like this hello-world image)
-        // let mut ran = has_ran_test.lock().unwrap();
-        // *ran = true;
     });
-
-    // let ran = has_ran.lock().unwrap();
-    // assert!(*ran);
 }
