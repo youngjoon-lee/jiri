8b121d442381cdc1e1c17a2e70453b33e05ed250
Youngjoon Lee <taxihighway@gmail.com>
2023-05-11 10:42:34 +0900

    use my fork of egui and import jiri-wasm

--- jiri-wasm/egui/src/app.rs
+++ jiri-wasm/egui/src/app.rs
@@ -1,7 +1,9 @@
+use jiri_wasm;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
+pub struct JiriWebApp {
     // Example stuff:
     label: String,
 
@@ -10,7 +12,7 @@ pub struct TemplateApp {
     value: f32,
 }
 
-impl Default for TemplateApp {
+impl Default for JiriWebApp {
     fn default() -> Self {
         Self {
             // Example stuff:
@@ -20,7 +22,7 @@ impl Default for TemplateApp {
     }
 }
 
-impl TemplateApp {
+impl JiriWebApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -32,11 +34,16 @@ impl TemplateApp {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
 
+        jiri_wasm::start(
+            "/ip4/127.0.0.1/tcp/9091/ws/p2p/12D3KooWSTiScugFjjNxJcL7GqVvDHDvWkiSNYfRMZ2iFvXNZuiA"
+                .to_string(),
+        );
+
         Default::default()
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for JiriWebApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
